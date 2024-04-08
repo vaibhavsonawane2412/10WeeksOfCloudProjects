0079c3ae4a78b09f7dbd3991a8ef998390c072ca
Vaibhav Sonawane <105963235+vaibhavsonawane2412@users.noreply.github.com>
2024-04-08 13:35:17 +0530

    Add files via upload

---
+++ Week 1 /Application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+  DB_HOST: "database-1.cluster-ch2oai26q4q5.ap-south-1.rds.amazonaws.com",
+  DB_USER: "admin",
+  DB_PWD: "Vaibhav2412",
+  DB_DATABASE: "webappdb",
+});
